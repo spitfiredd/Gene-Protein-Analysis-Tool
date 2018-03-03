e61c5ae7f554dfd8fa1c1c6daa50c5559b4c5bc4
Daniel Donovan <spitfiredd@gmail.com>
2018-03-03 10:25:55 -0500

    updates

--- analytics/dna_analysis.py
+++ analytics/dna_analysis.py
@@ -1,27 +1,66 @@
 """provides analysis on DNA sequences"""
+from itertools import zip_longest
 
 
-def ATGC_content(DNA):
-    """returns ATGC contents in the form of a dict"""
-    ATGCDict = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
-    for nucleotide in DNA:
-        try:
-            ATGCDict[nucleotide] += 1
-        except KeyError:
-            pass
-    return ATGCDict
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 
-def complementary(DNA):
-    """Given a DNA sequence, will return a complementary DNA sequence"""
-    cDNA = []
-    complementary = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-    for nucleotide in DNA:
-        try:
-            cDNA.append(complementary[nucleotide])
-        except KeyError:
-            cDNA.append('X')
-    return cDNA
+class DnaString:
+    complement_dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    contents = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
+    rna = {'A': 'A', 'T': 'U', 'G': 'G', 'C': 'C'}
+    codontable = {
+        'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+        'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+        'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+        'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+        'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+        'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+        'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+        'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
+    }
+
+    @classmethod
+    def update(cls, dnastring):
+        for nucleotide in dnastring:
+            cls.contents[nucleotide] += 1
+
+    def __init__(self, dnastring):
+        self.dnastring = dnastring
+        self.update(dnastring)
+
+    def complement(self):
+        st = ''
+        for nucleotide in self.dnastring:
+            st += self.complement_dict[nucleotide]
+        return st
+
+    def reverse_complement(self):
+        return self.complement()[::-1]
+
+    def rna_translation(self):
+        st = ''
+        for nucleotide in self.dnastring:
+            st += self.rna[nucleotide]
+        return st
+
+    def cg_pct(self):
+        total = sum(self.contents.values())
+        gc = 0
+        for key, val in self.contents.items():
+            if key in ('GC'):
+                gc += val
+        return gc / total
 
 
 def hamming_differences(DNA1, DNA2):
